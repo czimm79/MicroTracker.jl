90e6f8c83ccb957d08ac02328b7494f9621e0601
Documenter.jl <documenter@juliadocs.github.io>
2022-08-29 15:30:58 +0000

    build based on 7930d17

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Microbot","category":"page"},{"location":"#Microbot","page":"Home","title":"Microbot","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Microbot.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Microbot]","category":"page"}]
+}
