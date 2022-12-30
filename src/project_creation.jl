182a545698ae0cf7b93d5b4e8a9d51f913a8951b
Coy Zimmermann <czimmermann@mines.edu>
2022-12-30 11:59:03 -0700

    add first version of project creation
    testing only includes the version without example video. testing git clone is not feasible.

--- src/project_creation.jl
+++ src/project_creation.jl
@@ -1,9 +1,22 @@
-function create_project(folder::AbstractString, include_examples=true)
-    mkdir(folder)
-    mkdir("$folder/imagej_data")
-    mkdir("$folder/linked_results")
-    mkdir("$folder/original_video")
+using Git
 
+"""
+    create_project(folder::String, include_examples=true)
+
+Create the project!
+"""
+function create_project(folder::String; include_examples=true)
+    if include_examples
+        run(git(["clone", "https://github.com/czimm79/ExampleMicroTrackerData"]))
+        # need to make the include examples look the same as the one without
+    
+    else
+        mkdir(folder)
+        mkdir("$folder/imagej_data")
+        mkdir("$folder/linked_results")
+        mkdir("$folder/original_video")
+
+    end
 end
 
 #b = download("https://github.com/czimm79/ExampleMicroTrackerData/blob/main/original_video/aero_1_0714")
