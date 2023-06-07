a9df666dabd575961c32bfa6d6ee8ad72a898f25
Coy Zimmermann <coy.zimmermann79@gmail.com>
2023-06-07 16:52:55 -0600

    again

--- src/linking.jl
+++ src/linking.jl
@@ -1,5 +1,5 @@
 # Main hurdle is incorporating a small python version in this package - should do a micro test
-using PythonCall, CondaPkg
+using PythonCall#, CondaPkg
 
 # See PythonCall docs on how this was set up
 # https://cjdoris.github.io/PythonCall.jl/stable/pythoncall/#pythoncall-config
