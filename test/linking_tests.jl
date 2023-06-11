2c4d7e9d13392ad8727151ad2285717bbb7e2fe8
Coy Zimmermann <coy.zimmermann79@gmail.com>
2023-06-11 16:15:29 -0600

    fixed test error

--- test/linking_tests.jl
+++ test/linking_tests.jl
@@ -1,4 +1,5 @@
 @testset "particle data processing" begin
-    particle_csvs = cd(MicroTracker.get_particle_csvs, "assets")  # load test data from assets
+    assets_path = joinpath(dirname(pwd()), "assets")
+    particle_csvs = cd(MicroTracker.get_particle_csvs, assets_path)  # load test data from assets
     @test particle_csvs == ["5_13p5_61p35.csv", "5_8p4_28p68.csv"]
 end
